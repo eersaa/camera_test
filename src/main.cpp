6d101ec55ecf5b0eab21931f55ddaae878752b38
eersaa <eerik.saarinen@gmail.com>
2024-11-30 20:52:15 +0200

    Use rpipico specific cameratest example

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,77 @@
 #include <Arduino.h>
-/*
-Example for Adafruit_OV7670 library. With an ILI9341 TFT shield and
-OV7670 camera connected to Grand Central, displays a continuous live
-feed. That's all, keeping this a fairly minimal example.
-US
-HARDWARE REQUIRED:
-- Adafruit Grand Central board
-- Adafruit 2.8" TFT Touch Shield (touch is NOT USED in this example)
-- OV7670 camera w/2.2K pullups to SDA+SCL, 3.3V+GND wires to shield
-*/
+// Camera test for Pico RP2040 + ST7789 240x240 display.
+// Eventually this might get merged into existing cameratest sketch
+// so the same sketch compiles for either SAMD51 or RP2040, but at
+// the moment this diverges a lot (different display, etc.), maybe
+// later when we have solid RP2040 Feather support, a good camera
+// adapter PCB and some other things firm up.
+// REQUIRES EARLE PHILHOWER RP2040 BOARD PACKAGE - Arduino mbed
+// one won't work.
 
-#include <Wire.h>             // I2C comm to camera
-#include "Adafruit_OV7670.h"  // Camera library
-#include "Adafruit_ILI9341.h" // TFT display library
+#include <Wire.h>            // I2C comm to camera
+#include "Adafruit_OV7670.h" // Camera library
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
 
 // CAMERA CONFIG -----------------------------------------------------------
 
-#if defined(__SAMD51__) // Grand Central or other M4 boards
-// Set up arch and pins structures for Grand Central's SAMD51.
-// PCC data pins are not configurable and do not need specifying.
-OV7670_arch arch = {.timer = TCC1, .xclk_pdec = false};
-OV7670_pins pins = {.enable = PIN_PCC_D8, .reset = PIN_PCC_D9,
-                    .xclk = PIN_PCC_XCLK};
-#define CAM_I2C Wire1 // Second I2C bus next to PCC pins
-#endif // end __SAMD51__
+#if defined(PICO_SDK_VERSION_MAJOR)
+#include "hardware/spi.h"
 
-#define CAM_SIZE OV7670_SIZE_DIV2 // QVGA (320x240 pixels)
+OV7670_arch arch;
+OV7670_pins pins = {
+  .enable = -1, // Also called PWDN, or set to -1 and tie to GND
+  .reset  = 14, // Cam reset, or set to -1 and tie to 3.3V
+  .xclk   = 13, // MCU clock out / cam clock in
+  .pclk   = 10, // Cam clock out / MCU clock in
+  .vsync  = 11, // Also called DEN1
+  .hsync  = 12, // Also called DEN2
+  .data   = {2, 3, 4, 5, 6, 7, 8, 9}, // Camera parallel data out
+  .sda    = 20, // I2C data
+  .scl    = 21, // I2C clock
+};
+
+#define CAM_I2C Wire
+
+#endif
+
+#define CAM_SIZE OV7670_SIZE_DIV4 // QQVGA (160x120 pixels)
 #define CAM_MODE OV7670_COLOR_RGB // RGB plz
 
 Adafruit_OV7670 cam(OV7670_ADDR, &pins, &CAM_I2C, &arch);
+// Adafruit_OV7670 cam(0x30, &pins, &CAM_I2C, &arch); // OIV2640 WIP
 
-// SHIELD AND DISPLAY CONFIG -----------------------------------------------
+// DISPLAY CONFIG ----------------------------------------------------------
 
-// TFT shield pinout. Switch these if using Feather (DC=10, CS=9)
-#define TFT_DC 9
-#define TFT_CS 10
-#define TFT_SPI SPI
+#define TFT_CS  17 // Near SPI0 at south end of board
+#define TFT_DC  16
+#define TFT_RST -1 // Connect to MCU reset
 
-// Screen DMA allows faster updates, but USE_SPI_DMA must be
-// manually enabled in Adafruit_SPITFT.h (it's not on by default
-// on Grand Central). If DMA is not enabled there, a helper class
-// makes screen writes a little bit faster (but not in background
-// like with DMA).
-#if !defined(USE_SPI_DMA)
-#include "SPIBrute.h" // Direct-to-SPI-registers helper class for SAMD51
-#endif
-
-Adafruit_ILI9341 tft(&TFT_SPI, TFT_DC, TFT_CS);
-#if defined(USE_SPI_BRUTE)
-SPIBrute brute(&TFT_SPI);
-#endif
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 // SETUP - RUNS ONCE ON STARTUP --------------------------------------------
 
-void setup() {
+void setup(void) {
   Serial.begin(9600);
-  //while (!Serial);
-  Serial.println("Hello");
+  //while(!Serial);
+  delay(1000);
+  Serial.println(F("Hello! Camera Test"));
 
-  // 50 MHz to screen is OK if wiring is clean (e.g. shield or FeatherWing).
-  // Otherwise (if using jumper wires to screen), stick to 24 MHz.
-#if defined(__SAMD51__)
-  TFT_SPI.setClockSource(SERCOM_CLOCK_SOURCE_100M);
-  tft.begin(50000000);
-#else
-  tft.begin(24000000);
-#endif
-  tft.setRotation(3); // Match camera orientation on Grand Central
-  tft.fillScreen(ILI9341_BLACK);
-#if defined(USE_SPI_BRUTE)
-  brute.begin();
-#endif
+  pinMode(25, OUTPUT);
+  digitalWrite(25, LOW);
 
+  // These are currently RP2040 Philhower-specific
+  SPI.setSCK(18); // SPI0
+  SPI.setTX(19);
+  Wire.setSDA(pins.sda); // I2C0
+  Wire.setSCL(pins.scl);
+
+  tft.init(240, 240);
+  tft.setSPISpeed(48000000);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.println("Howdy");
+tft.setRotation(3);
   // Once started, the camera continually fills a frame buffer
   // automagically; no need to request a frame.
   OV7670_status status = cam.begin(CAM_MODE, CAM_SIZE, 30.0);
@@ -80,10 +80,20 @@ void setup() {
     for(;;);
   }
 
+#if 0
+// OV2640 WIP
+  cam.writeRegister(0xFF, 0x01);
+  uint8_t pid = cam.readRegister(0x0A); // Should be 0x26
+  uint8_t ver = cam.readRegister(0x0B); // Should be 0x41
+  Serial.println(pid, HEX);
+  Serial.println(ver, HEX);
+#else
   uint8_t pid = cam.readRegister(OV7670_REG_PID); // Should be 0x76
   uint8_t ver = cam.readRegister(OV7670_REG_VER); // Should be 0x73
   Serial.println(pid, HEX);
   Serial.println(ver, HEX);
+#endif
+//  cam.test_pattern(OV7670_TEST_PATTERN_COLOR_BAR);
 }
 
 // MAIN LOOP - RUNS REPEATEDLY UNTIL RESET OR POWER OFF --------------------
@@ -98,6 +108,9 @@ void setup() {
 uint16_t frame = KEYFRAME; // Force 1st frame as keyframe
 
 void loop() {
+
+  gpio_xor_mask(1 << 25); // Toggle LED each frame
+
   // This was for empirically testing window settings in src/arch/ov7670.c.
   // Your code doesn't need this. Just keeping around for future reference.
   if(Serial.available()) {
@@ -105,20 +118,17 @@ void loop() {
     uint32_t hstart = Serial.parseInt();
     uint32_t edge_offset = Serial.parseInt();
     uint32_t pclk_delay = Serial.parseInt();
+    while(Serial.read() >= 0); // Delete line ending or other cruft
     OV7670_frame_control((void *)&cam, CAM_SIZE, vstart, hstart,
                          edge_offset, pclk_delay);
   }
 
   if (++frame >= KEYFRAME) { // Time to sync up a fresh address window?
     frame = 0;
-#if defined(USE_SPI_DMA)
-    tft.dmaWait(); // Wait for prior transfer to complete
-#elif defined(USE_SPI_BRUTE)
-    brute.wait();
-#endif
+
     tft.endWrite();   // Close out prior transfer
     tft.startWrite(); // and start a fresh one (required)
-    // Address window centers QVGA image on screen. NO CLIPPING IS
+    // Address window centers QQVGA image on screen. NO CLIPPING IS
     // PERFORMED, it is assumed here that the camera image is equal
     // or smaller than the screen.
     tft.setAddrWindow((tft.width() - cam.width()) / 2,
@@ -127,9 +137,9 @@ void loop() {
   }
 
   // Pause the camera DMA - hold buffer steady to avoid tearing
-  cam.suspend();
+//  cam.suspend();
 
-  //cam.capture(); // Manual capture instead of PCC DMA
+  //cam.capture(); // Manual capture instead of PIO DMA
 
   // Postprocessing effects. These modify a previously-captured
   // image in memory, they are NOT in-camera effects.
@@ -147,16 +157,9 @@ void loop() {
 
   // Camera data arrives in big-endian order...same as the TFT,
   // so data can just be issued directly, no byte-swap needed.
-  // Both the DMA and brute cases handle this.
-#if defined(USE_SPI_DMA)
-  tft.dmaWait();
   tft.writePixels(cam.getBuffer(), cam.width() * cam.height(), false, true);
-#elif defined(USE_SPI_BRUTE)
-  brute.wait();
-  brute.write((uint8_t *)cam.getBuffer(), cam.width() * cam.height() * 2);
-#else
-  tft.writePixels(cam.getBuffer(), cam.width() * cam.height(), false, true);
-#endif
+  // To do: add a DMA SPI transfer here -- even if it's blocking,
+  // should still go a ton faster due to endian match.
 
-  cam.resume(); // Resume DMA to camera buffer
+//  cam.resume(); // Resume DMA to camera buffer
 }
